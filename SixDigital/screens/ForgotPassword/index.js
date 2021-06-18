5cac6b0c9a15c0a77c151f8c7f6c8f3a24275066
Palak Soni <palaksoni1895@gmail.com>
2021-06-18 13:54:58 -0400

    Added Admin Screen

--- SixDigital/screens/ForgotPassword/index.js
+++ SixDigital/screens/ForgotPassword/index.js
@@ -1,21 +1,87 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 
 import {
-  SafeAreaView,
+  Dimensions,
+  TouchableWithoutFeedback,
   ScrollView,
-  StatusBar,
+  ImageBackground,
   StyleSheet,
+  Image,
+  Button,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import TextField from '../../components/TextField';
+import * as UserActions from '../../store/actions/UserActions';
 import color from '../../style/color';
+import {SCREEN_HEIGHT, SCREEN_WIDTH} from '../../style/fontSize';
 
 const ForgotPassword = ({navigation}) => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const onPressHandler = () => {
+    dispatch(UserActions.loginUser(email, password));
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.splashTxt}>forgot password</Text>
+      <ImageBackground
+        style={{flex: 1, resizeMode: 'cover', justifyContent: 'center'}}
+        source={require('../../assets/login/login_bg.png')}>
+        <ScrollView>
+          <View style={styles.forgotPwdContainer}>
+          <View style={styles.inputfieldContainer}>
+              <TextField
+                value={email}
+                onChangeText={setEmail}
+                label="Email"
+                keyboardType="email"
+                flex={1}
+              />
+            </View>
+            <View style={styles.inputfieldContainer}>
+              <TextField
+                value={email}
+                onChangeText={setEmail}
+                label="Current Password"
+                keyboardType="password"
+                flex={1}
+                secureTextEntry={true}
+              />
+            </View>
+            <View  style={styles.inputfieldContainer}>
+              <TextField
+                value={email}
+                onChangeText={setEmail}
+                label="New Password"
+                keyboardType="password"
+                flex={1}
+                secureTextEntry={true}
+              />
+            </View>
+            <View  style={styles.inputfieldContainer}>
+              <TextField
+                value={password}
+                onChangeText={setPassword}
+                label="Confirm Password"
+                keyboardType="password"
+                secureTextEntry={true}
+                flex={1}
+              />
+            </View>
+            <View  style={styles.inputfieldContainer}>
+              <Button
+                title="Submit"
+                color={color.primary}
+                onPress={onPressHandler}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </ImageBackground>
     </View>
   );
 };
@@ -23,16 +89,22 @@ const ForgotPassword = ({navigation}) => {
 export default ForgotPassword;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: color.purple,
-      alignItems:'center',
-      justifyContent:'center'
-    },
-    splashTxt:{
-        color:color.white,
-        fontWeight:'bold',
-        fontSize:40
-    }
-  });
-  ;
+  container: {
+    flex: 1,
+  },
+  forgotPwdContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent:'space-between',
+    paddingHorizontal: SCREEN_WIDTH * 0.1,
+    marginVertical:SCREEN_HEIGHT * 0.2
+  },
+  inputfieldContainer:{
+    marginVertical: SCREEN_HEIGHT * 0.02
+  },
+  input: {
+    height: SCREEN_HEIGHT * 0.05,
+    borderWidth: 1,
+    borderColor: color.lightGrey,
+  },
+});
---
+++ SixDigital/screens/admin/MyClients/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import color from '../../../style/color';
+import DrawerHeader from '../../../components/DrawerHeader';
+import ClientList from './components/ClientList';
+
+const MyClients = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <DrawerHeader />
+        <ClientList />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MyClients;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: color.white,
+  },
+});
